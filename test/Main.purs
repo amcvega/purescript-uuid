1f197041899f9619c08cb007d03e4103afb84c09
amcvega <me@amcvega.com>
2019-03-07 10:24:33 +0800

    Fixed test to use main

--- test/Main.purs
+++ test/Main.purs
@@ -1,12 +1,14 @@
 module Test.Main where
 
-import Prelude (Unit, discard, show, ($), (<))
+import Prelude
+
 import Effect (Effect)
 import Effect.Console (log)
 import Effect.Aff.AVar
 
 import Data.Uuid
 import Data.Maybe
+import Data.NonEmpty (NonEmpty(..))
 
 import Data.Array as Array
 import Data.Int.Bits
@@ -16,7 +18,9 @@ import Test.Unit.Main (runTest)
 -- import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.QuickCheck (quickCheck, quickCheck')
 
-import Test.QuickCheck (Result(..), (===))
+import Test.QuickCheck (Result(..), (===), (/==), arbitrary)
+import Test.QuickCheck.Gen (Gen(..))
+import Test.QuickCheck.Gen as Gen
 
 v4HiProperty :: Int -> Int -> Int -> Int -> Result
 v4HiProperty i1 i2 i3 i4 =
@@ -31,11 +35,22 @@ v4VersionProperty i1 i2 i3 i4 =
   in ((x `zshr` 12) .&. 0x0000000f) === 4
 
 
+data UuidArg = IsUuid String
+             | NotUuid String
 
-validProperty :: Int -> Int -> Int -> Int -> Result
-validProperty i1 i2 i3 i4 =
-  let uid = uuidV4 i1 i2 i3 i4
-  in isValid (show uid) === true
+isValidProperty :: Gen Result
+isValidProperty = do
+  args <- uuidArgs
+  pure $ case args of
+    IsUuid s -> isValid s === true
+    NotUuid s -> isValid s /== true
+  where
+    uuidArgs = do
+      let uid = uuidV4 <$> arbitrary <*> arbitrary
+                <*> arbitrary <*> arbitrary
+          uid' = (IsUuid <<< show) <$> uid
+          s = NotUuid <$> arbitrary
+      Gen.oneOf $ NonEmpty (uid') [uid', s]
 
 
 parseProperty :: Int -> Int -> Int -> Int -> Result
@@ -48,7 +63,8 @@ parseProperty i1 i2 i3 i4 =
 
 main :: Effect  Unit
 main = runTest do
-  test "valid property" $ quickCheck validProperty
+  -- test "valid property" $ quickCheck validProperty
+  test "isValid" $ quickCheck isValidProperty
   test "parse property" $ quickCheck parseProperty
   test "v4 Version property" $ quickCheck v4VersionProperty
   test "v4 Hi property" $ quickCheck v4HiProperty  
