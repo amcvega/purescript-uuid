7c6e340b5ecdd316550016f4b6ba110d9e071bab
Miguel Vega <me@amcvega.com>
2021-06-23 09:55:59 +0800

    Removed array from implementation

--- src/Data/Uuid.purs
+++ src/Data/Uuid.purs
@@ -44,7 +44,8 @@ instance jsonDecodeUuid :: DecodeJson Uuid where
 
 instance eqUuid :: Eq Uuid where
   eq (Uuid x1 x2 x3 x4) (Uuid y1 y2 y3 y4) =
-    [x1,x2,x3,x4] == [y1,y2,y3,y4]
+    (x1 == y1) && (x2 == y2) && (x3 == y3) && (x4 == y4)
+    -- [x1,x2,x3,x4] == [y1,y2,y3,y4]
 
 
 -- | Create nil UUID. (all 0s)
